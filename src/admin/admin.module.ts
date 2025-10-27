257bb17628a98a56104d0a915902210f3fc947b4
yuii2908 <sayrung.j@aware.co.th>
2025-10-28 03:51:04 +0700

    fix: admin get conversion and withdraw

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -9,15 +9,26 @@ import {
   UserAdminSchema,
 } from './user-admin/schemas/user-admin.schema';
 import { JwtService } from '@nestjs/jwt';
+import { Withdraw, WithdrawSchema } from 'src/withdraw/schemas/withdraw.schema';
+import { InvolveService } from 'src/involve/involve.service';
+import { CacheModule } from '@nestjs/cache-manager';
+import { Offer, OfferSchema } from 'src/offer/schemas/offer.schema';
+import { Deeplink, DeeplinkSchema } from 'src/involve/schemas/deeplink.schema';
+import { User, UserSchema } from 'src/user/schemas/user.schema';
 
 @Module({
   imports: [
+    CacheModule.register(),
     InvolveModule,
     MongooseModule.forFeature([
       { name: UserAdmin.name, schema: UserAdminSchema },
+      { name: Withdraw.name, schema: WithdrawSchema },
+      { name: Offer.name, schema: OfferSchema },
+      { name: Deeplink.name, schema: DeeplinkSchema },
+      { name: User.name, schema: UserSchema },
     ]),
   ],
   controllers: [AdminController],
-  providers: [AdminService, UserAdminService, JwtService],
+  providers: [AdminService, UserAdminService, JwtService, InvolveService],
 })
 export class AdminModule {}
