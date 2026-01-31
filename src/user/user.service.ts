7375ed3bab5831af16a935dfc6c3bb409a2d39b1
yui <jaibun.s89@gmail.com>
2026-02-01 02:15:20 +0700

    fix: withdraw

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -124,19 +124,34 @@ export class UserService {
     //     .lean();
     // }
 
+   
     const mobileData = user?.mobile?.includes('+66')
       ? user?.mobile?.slice(3)
       : user?.mobile;
     const mobile = '0' + mobileData;
 
-    const myCashbackDataList = await this.userMyCashbacksModel
-      .find({
-        $or: [{ email: user.email }, { phoneNumber: user.mobile }, { phoneNumber: mobile }],
-      })
-      .lean();
-      
+    // const myCashbackDataList = await this.userMyCashbackModel
+    //   .find({
+    //     $or: [{ email: user.email }, { phoneNumber: user.mobile }, { phoneNumber: mobile }],
+    //   })
+    //   .lean();
+
+    let myCashbackDataList = [];
+    if (user?.mobile) {
+      myCashbackDataList = await this.userMyCashbacksModel
+        .find({
+          $or: [{ phoneNumber: user.mobile }, { phoneNumber: mobile }],
+        })
+        .lean();
+    }
+
     if (myCashbackDataList?.length < 1) {
-      throw new UnauthorizedException({ message: 'User not found' });
+      myCashbackDataList = await this.userMyCashbacksModel
+        .find({
+          email: { $regex: user.email, $options: 'i' }, // Use $regex for case-insensitive search on user.email
+          // $or: [{ email: user.email }, { phoneNumber: user.mobile }, { phoneNumber: mobile }],
+        })
+        .lean();
     }
     
 
