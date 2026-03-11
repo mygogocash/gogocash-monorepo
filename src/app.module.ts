6e9ca854e6065a04d9ce4192e0556187ef0ce698
yui <jaibun.s89@gmail.com>
2026-03-11 23:39:58 +0700

    fix: add

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,29 +6,30 @@ import { AuthModule } from './auth/auth.module';
 import envConfig from './config/env.config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
+import { AdminModule } from './admin/admin.module';
 import { OfferModule } from './offer/offer.module';
 import { WithdrawModule } from './withdraw/withdraw.module';
 import { GoogleDriveModule } from './google-drive/google-drive.module';
 import { PointModule } from './point/point.module';
 import { TelegramBotModule } from './telegram-bot/telegram-bot.module';
 import { ScheduleModule } from '@nestjs/schedule';
-import { AnalyticsModule } from './analytics/analytics.module';
+import { InvolveModule } from './involve/involve.module';
 @Module({
   imports: [
     ScheduleModule.forRoot(),
-    AnalyticsModule,
     ConfigModule.forRoot({
       load: [envConfig],
     }),
     MongooseModule.forRoot(process.env.MONGO_URI!),
     AuthModule,
     UserModule,
-    AuthModule,
+    AdminModule,
     OfferModule,
     WithdrawModule,
     GoogleDriveModule,
     PointModule,
     TelegramBotModule,
+    InvolveModule,
   ],
   controllers: [AppController],
   providers: [AppService],
