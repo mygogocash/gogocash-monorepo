821b14982205ad4cd5c33b4a8a1184c1dd69bc22
yui <jaibun.s89@gmail.com>
2026-03-11 23:34:14 +0700

    fix: admin module

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,52 +1,56 @@
 /* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { UserService } from 'src/user/user.service';
-import { MongooseModule } from '@nestjs/mongoose';
-import { User, UserSchema } from 'src/user/schemas/user.schema';
-import { JwtModule, JwtService } from '@nestjs/jwt';
-import { Point } from 'src/point/entities/point.entity';
-import { PointSchema } from 'src/point/schemas/point.schema';
-import {
-  UserMyCashback,
-  UserMyCashbackSchema,
-} from 'src/user/schemas/user-my-cashback.schema';
-import { MailerModule } from '@nestjs-modules/mailer';
-import { UserOtp, UserOtpSchema } from 'src/user/schemas/user-otp.schema';
-import { AuthService } from 'src/auth/auth.service';
-import { OtpService } from 'src/auth/otp.service';
+import { AdminService } from './admin.service';
 import { AdminController } from './admin.controller';
+import { InvolveModule } from 'src/involve/involve.module';
+import { UserAdminService } from './user-admin/user-admin-service';
+import { MongooseModule } from '@nestjs/mongoose';
+import {
+  UserAdmin,
+  UserAdminSchema,
+} from './user-admin/schemas/user-admin.schema';
+import { JwtService } from '@nestjs/jwt';
+import { Withdraw, WithdrawSchema } from 'src/withdraw/schemas/withdraw.schema';
+import { InvolveService } from 'src/involve/involve.service';
+import { CacheModule } from '@nestjs/cache-manager';
+import { Offer, OfferSchema } from 'src/offer/schemas/offer.schema';
+import { Deeplink, DeeplinkSchema } from 'src/involve/schemas/deeplink.schema';
+import { User, UserSchema } from 'src/user/schemas/user.schema';
+import { FeeRate, FeeRateSchema } from 'src/withdraw/schemas/feeRate.schema';
+import { GoogleDriveService } from 'src/google-drive/google-drive.service';
+import { UserMyCashback, UserMyCashbackSchema } from 'src/user/schemas/user-my-cashback.schema';
+import { Category, CategorySchema } from 'src/offer/schemas/category.schema';
+import { Conversion, ConversionSchema } from 'src/withdraw/schemas/conversion.schema';
+import { Banner, BannerSchema } from 'src/offer/schemas/banner.schema';
+import { UserService } from 'src/user/user.service';
+import { JobService } from 'src/withdraw/cronjob/job.service';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
+    CacheModule.register(),
+    InvolveModule,
     MongooseModule.forFeature([
+      { name: UserAdmin.name, schema: UserAdminSchema },
+      { name: Withdraw.name, schema: WithdrawSchema },
+      { name: Offer.name, schema: OfferSchema },
+      { name: Deeplink.name, schema: DeeplinkSchema },
       { name: User.name, schema: UserSchema },
-      { name: Point.name, schema: PointSchema },
+      { name: FeeRate.name, schema: FeeRateSchema },
       { name: UserMyCashback.name, schema: UserMyCashbackSchema },
-      { name: UserOtp.name, schema: UserOtpSchema },
+      { name: Category.name, schema: CategorySchema },
+      { name: Conversion.name, schema: ConversionSchema },
+      { name: Banner.name, schema: BannerSchema },
     ]),
-    JwtModule.register({
-      // This is for signing tokens your backend generates.
-      // For verifying Crossmint's tokens, you'll use a separate verification process.
-      secret: process.env.CROSSMINT_SECRET, // Your own secret for tokens you issue
-      signOptions: { expiresIn: '60m' },
-    }),
-    MailerModule.forRoot({
-      transport: {
-        host: process.env.SMTP_HOST, // Change from 'localhost' to 'smtp.sendgrid.net'
-        port: 587, // Use 587 (TLS) or 465 (SSL)
-        secure: false, // true for 465, false for 587
-        auth: {
-          user: process.env.SMTP_USER,
-          pass: process.env.SMTP_PASS,
-        },
-      },
-    }),
   ],
   controllers: [AdminController],
-  providers: [AuthService, UserService, JwtService, OtpService],
+  providers: [
+    AdminService,
+    UserAdminService,
+    JwtService,
+    InvolveService,
+    GoogleDriveService,
+    UserService,
+    JobService,
+  ],
 })
-export class AuthModule {}
+export class AdminModule {}
